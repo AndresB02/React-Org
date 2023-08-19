6f8cd6cdfa859da4d9219ddddce827bca57c97e2
Andres Buitrago <a.buitrago02@gmail.com>
2023-08-18 22:34:37 -0500

    Se agregaron bibliotecas con hex-to-rgba, react-icons y se agrego uuid para dar identificador a los elementos

--- src/components/Colaborador/Colaborador.js
+++ src/components/Colaborador/Colaborador.js
@@ -1,10 +1,15 @@
 import "./Colaborador.css";
-
+import { AiFillCloseCircle } from "react-icons/ai";
+// al importar el AiFillCloseCircle que es el icono de react
+//se puede llamar como una etiqueta de html para incorporarla
 const Colaborador = (props) => {
   const { nombre, puesto, foto, equipo } = props.datos;
-  const { colorPrimario } = props;
+  const { colorPrimario, eliminarColaborador } = props;
   return (
     <div className="colaborador">
+      <AiFillCloseCircle className="eliminar" onClick={eliminarColaborador}>
+        D
+      </AiFillCloseCircle>
       <div className="encabezado" style={{ backgroundColor: colorPrimario }}>
         <img src={foto} alt={nombre} />
       </div>
--- src/components/Equipo/Equipo.js
+++ src/components/Equipo/Equipo.js
@@ -1,13 +1,14 @@
 import "./Equipo.css";
 import Colaborador from "../Colaborador/Colaborador";
+import hexToRgba from "hex-to-rgba";
 //import hexToRgba from "he
 //ahora aca en equipo es el que estara recinbiendo los datos
 const Equipo = (props) => {
   //Destructuracion
-  const { colorPrimario, colorSecundario, titulo } = props.datos;
+  const { colorPrimario, colorSecundario, titulo, id } = props.datos;
   const { colaboradores, eliminarColaborador, actualizarColor } = props;
   const obj = {
-    backgroundColor: colorSecundario,
+    backgroundColor: hexToRgba(colorPrimario, 0.3),
   };
 
   const estiloTitulo = { borderColor: colorPrimario };
@@ -16,6 +17,14 @@ const Equipo = (props) => {
     <>
       {colaboradores.length > 0 && (
         <section className="equipo" style={obj}>
+          <input
+            type="color"
+            className="input-color"
+            value={colorPrimario}
+            onChange={(e) => {
+              actualizarColor(e.target.value, id);
+            }}
+          />
           <h3 style={estiloTitulo}>{titulo}</h3>
           <div className="colaboradores">
             {colaboradores.map((colaborador, index) => (
@@ -23,6 +32,7 @@ const Equipo = (props) => {
                 datos={colaborador}
                 key={colaborador.nombre}
                 colorPrimario={colorPrimario}
+                eliminarColaborador={eliminarColaborador}
               />
             ))}
           </div>
